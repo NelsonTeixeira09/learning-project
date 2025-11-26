00a49cbb4bc22236fadf09b825656a1d7ba98cad
NelsonTeixeira09 <nelson.teixeira@azoreshive.com>
2025-11-26 12:09:49 -0100

    Atualização 11/26/2025

---
+++ Lessons/functions.js
@@ -0,0 +1,17 @@
+// function createTag(name, prefix, sufix) {
+//     if(prefix && sufix){
+//         return `${prefix} ${name} ${sufix}`;
+//     }if (prefix) {
+//         return `${prefix} ${name}`;
+//     }
+//     return name;
+// }
+
+// const tag = createTag("Nelson","[Programmer]","Dev");
+// console.log(tag);
+
+function pow(number, exponent) {
+    return number ** exponent;
+}
+
+console.log(pow(2,4));
