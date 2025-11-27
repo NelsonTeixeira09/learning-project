5ee562b22967cd714581375f4a5619105f8a6b34
Nelson Teixeira <nelsonteixeira@MacBook-Air-de-Nelson.local>
2025-11-27 09:33:53 -0100

    new commit

---
+++ Lessons/Functions/functions.js
@@ -0,0 +1,24 @@
+function createTag(name, prefix, sufix) {
+    if(prefix && sufix){
+        return `${prefix} ${name} ${sufix}`;
+    }if (prefix) {
+        return `${prefix} ${name}`;
+    }
+    return name;
+}
+
+const tag = createTag("Nelson","[Programmer]","Dev");
+console.log(tag);
+
+function pow(number, exponent) {
+    return number ** exponent;
+}
+
+console.log(pow(2,4));
+
+
+function sum(a,b){
+    return a + b;
+}
+
+export { sum };
---
+++ index.js
@@ -0,0 +1,4 @@
+import { sum } from './Lessons/Functions/functions.js';
+
+const result = sum(5, 7);
+console.log(`The sum is: ${result}`);
