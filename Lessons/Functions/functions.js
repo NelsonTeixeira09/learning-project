6686dc9de71ff96d77ccfe335802a6e135547367
Nelson Teixeira <nelsonteixeira@MacBook-Air-de-Nelson.local>
2025-11-27 09:55:20 -0100

    new

--- Lessons/Functions/functions.js
+++ Lessons/Functions/functions.js
@@ -1,24 +1,22 @@
-function createTag(name, prefix, sufix) {
-    if(prefix && sufix){
-        return `${prefix} ${name} ${sufix}`;
-    }if (prefix) {
-        return `${prefix} ${name}`;
-    }
-    return name;
-}
+// function createTag(name, prefix, sufix) {
+//     if(prefix && sufix){
+//         return `${prefix} ${name} ${sufix}`;
+//     }if (prefix) {
+//         return `${prefix} ${name}`;
+//     }
+//     return name;
+// }
 
-const tag = createTag("Nelson","[Programmer]","Dev");
-console.log(tag);
+// const tag = createTag("Nelson","[Programmer]","Dev");
+// console.log(tag);
 
-function pow(number, exponent) {
-    return number ** exponent;
-}
+// function pow(number, exponent) {
+//     return number ** exponent;
+// }
 
-console.log(pow(2,4));
+// console.log(pow(2,4));
 
 
-function sum(a,b){
+export function sum(a,b){
     return a + b;
 }
-
-export { sum };
