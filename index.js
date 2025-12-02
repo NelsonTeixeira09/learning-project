372181aae0ec74e0e007116b32943ad88ca22497
Nelson Teixeira <nelsonteixeira@MacBook-Air-de-Nelson.local>
2025-12-02 15:49:50 -0100

    New Commit

--- index.js
+++
@@ -1,4 +0,0 @@
-import { sum } from './Lessons/Functions/functions.js';
-
-const result = sum(5, 7);
-console.log(`The sum is: ${result}`);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import { sum } from '../Lessons/Functions/functions.js';
+
+// const result = sum(5, 7);
+// console.log(`The sum is: ${result}`);
+
+console.log('Email:', process.env.email);
+console.log('Password:', process.env.password);
